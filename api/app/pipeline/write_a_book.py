9c10faeddcb7827ae686cfdebbfa71b82e5bb2b9
avidness <adavidness@gmail.com>
2025-01-21 00:37:07 -0500

    code cleanup

--- api/app/pipeline/write_a_book.py
+++ api/app/pipeline/write_a_book.py
@@ -3,44 +3,26 @@ from app.agents.BigPictureArchitect import BigPictureArchitect
 from app.services.neo4j.CharacterGraph import CharacterGraph
 
 async def run(user_input: str):
-  #yield "Beginning request..."
-
   try:
+      # Create an Outline
       agent = BigPictureArchitect(user_input)
       outline = await agent.generate_outline()
-      
       yield outline.to_json()
 
+      # Store Characters
       with CharacterGraph() as graph:
           for c in outline.characters:
             print(f"Adding character: {c.name}")
             graph.add_character(c)
             
-      print('adapter')
+      # Store Chapters
       adapter = ChapterAdapter()
-      print('adapter2')
-      
       for chap in outline.chapters:
-          print('Chapter', chap)
           existing_chapter = adapter.get_chapter_by_id(chap.chapter_id)
           if existing_chapter:
               adapter.update_chapter(chap)
-              print('Chapter updated', chap)
           else:
               adapter.upsert_chapter(chap)
-              print('Chapter created', chap)
                   
-            
-
-      '''
-      for chap in outline.chapters:
-        yield chap.to_json()
-
-      for char in outline.characters:
-        yield char.to_json()
-      '''
   except Exception as e:
       yield f"Error running core pipeline: {e}"
-
-  #async for result in await agent.generate_outline():
-  #  yield result
