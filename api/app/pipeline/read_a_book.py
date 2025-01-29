c3ae1e8952eada370ea425aec5bedd3a3bb528ed
avidness <adavidness@gmail.com>
2025-01-28 23:35:58 -0500

    tweak prompt to include object specs for character, location, relationship

--- api/app/pipeline/read_a_book.py
+++ api/app/pipeline/read_a_book.py
@@ -9,16 +9,38 @@ async def process_book(result: str):
 
     yield "Extracted file text -> Splitting into chunks" + str(len(chunks))
 
+    # store chunk in pinecone
+
     results = []
     for chunk in chunks[:2]:
         SummaryExtractor
 
         agent = SummaryExtractor(chunk)
         result = await agent.run()
+
+        json_obj = json.loads(result)
+        print(json_obj)
+        print(json_obj.themes)
+        print(json_obj.summary)
+        
+        print(json_obj.locations)
+        print(json_obj.characters)
+        print(json_obj.relationships)
+
+        # extract characters, relationships, locations, themes
+        # consolidate with existing characters, relationships, locations
+
+        # Consolidate logic - 
+        # search for name/alias results, 
+        # if exists, 
+        #   plug both versions into agent to rewrite a merged version 
+        
+        # store result in neo4j
         print(result)
+
         results.append(result)
 
     yield json.dumps({
         "filename": "result.filename",
-        "processed_data": chunks
+        "processed_data": result
     })
